2097029ee9b30a44406d857109093f4ace21ce91
Rohit suman <rohit.sangod74@gmail.com>
2025-11-19 10:51:18 +0530

    add post css debugs

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     plugins: {
+        'postcss-preset-env': {
+            stage: 0,
+            features: {
+                'custom-properties': false, // Preserve CSS variables
+            },
+        },
         autoprefixer: {},
     },
 };
