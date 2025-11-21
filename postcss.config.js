027dd455104c98fbdc09b1ed8076604eb056c30d
Rohit suman <rohit.sangod74@gmail.com>
2025-11-21 13:47:40 +0530

    update polaris version

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     plugins: {
-        'postcss-preset-env': {
-            stage: 0,
-            features: {
-                'custom-properties': false, // Preserve CSS variables
-            },
+      autoprefixer: {},
+      'postcss-preset-env': {
+        features: {
+          'calc': false,
         },
-        autoprefixer: {},
+      },
     },
-};
+  };
+  
