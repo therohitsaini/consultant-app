29cd8b97ea82b6e5f3e3398a072bdf08d2f5a204
Rohit suman <rohit.sangod74@gmail.com>
2026-01-13 13:23:15 +0530

    Update AdminMenu.js

--- src/pages/AdminMenu.js
+++ src/pages/AdminMenu.js
@@ -3,20 +3,20 @@ import { NavMenu, TitleBar } from '@shopify/app-bridge-react';
 import { useAppBridge } from '../components/createContext/AppBridgeContext';
 
 const AdminMenu = () => {
-  const app = useAppBridge();
-  if (!app) return <div>Loading...</div>; // prevent errors if App Bridge not initialized
-
-  return (
-    <>
-      <TitleBar title="Consultant App" />
-      <NavMenu>
-        <a href="/" rel="home">Home</a>
-        <a href="/dashboard">Dashboard</a>
-        <a href="/consultants">Consultants</a>
-        <a href="/settings">Settings</a>
-      </NavMenu>
-    </>
-  );
+    const app = useAppBridge();
+    if (!app) return <div>Loading...</div>; // prevent errors if App Bridge not initialized
+    console.log("app_____________", app);
+    return (
+        <>
+            <TitleBar title="Consultant App" />
+            <NavMenu>
+                <a href="/" rel="home">Home</a>
+                <a href="/dashboard">Dashboard</a>
+                <a href="/consultants">Consultants</a>
+                <a href="/settings">Settings</a>
+            </NavMenu>
+        </>
+    );
 };
 
 export default AdminMenu;
