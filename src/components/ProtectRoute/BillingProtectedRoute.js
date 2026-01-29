7a32d692205af57f50b2108c255867a1ac0c884c
Rohit suman <rohit.sangod74@gmail.com>
2026-01-29 18:32:55 +0530

    a

--- src/components/ProtectRoute/BillingProtectedRoute.js
+++ src/components/ProtectRoute/BillingProtectedRoute.js
@@ -1,18 +1,28 @@
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
 import { useAppStatusBilling } from "../ProtectRoute/AppStatusBillingProvider";
+import { useAppBridge } from "../createContext/AppBridgeContext";
+import { Redirect } from "@shopify/app-bridge/actions";
 
 export const BillingProtectedRoute = ({ children }) => {
     const billing = useAppStatusBilling();
+    const app = useAppBridge();
 
-    // Wait for provider and initial fetch
+    useEffect(() => {
+        if (!billing || billing.loading) return;
+
+        if (!billing.isPaid) {
+            const redirect = Redirect.create(app);
+
+            redirect.dispatch(
+                Redirect.Action.REMOTE,
+                "https://admin.shopify.com/store/rohit-12345839/charges/label-node/pricing_plans"
+            );
+        }
+    }, [billing, app]);
+
     if (!billing || billing.loading) return null;
 
-    const { isPaid } = billing;
-
-    // ACTIVE = paid → allow all routes. Not ACTIVE → show pricing only
-    if (!isPaid) {
-        return <Navigate to="/pricing" replace />;
-    }
+    if (!billing.isPaid) return null;
 
     return children;
 };
