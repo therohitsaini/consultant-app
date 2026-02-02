78864e90cdd6b554c21a9f09f241ebf15ff04982
Rohit <rohit.sangod74@gmail.com>
2026-02-03 00:01:30 +0530

    a

--- src/components/ConsultantDashboard/VideoCallingPage.js
+++ src/components/ConsultantDashboard/VideoCallingPage.js
@@ -326,11 +326,11 @@ function VideoCallingPage() {
             console.log("callAccepted___handleEndCall_____TransactionId", callAccepted);
             socket.emit("call-ended", { callerId: callerId, receiverId: receiverId, channel: channelNameParam, callType: callType, transactionId: callAccepted?.transactionId, shopId: "690c374f605cb8b946503ccb" });
             const returnUrl = params.get("returnUrl");
-            if (returnUrl) {
-                window.top.location.href = decodeURIComponent(returnUrl);
-            } else {
-                navigate(-1);
-            }
+            // if (returnUrl) {
+            //     window.top.location.href = decodeURIComponent(returnUrl);
+            // } else {
+            //     navigate(-1);
+            // }
         } else {
             console.log("No transaction ID, skipping emit");
             stopTimer();
