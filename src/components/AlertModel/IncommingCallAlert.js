59676707b2b073899e90609de34e8cdbffdea1c7
Rohit suman <rohit.sangod74@gmail.com>
2026-03-02 10:59:39 +0530

    a

--- src/components/AlertModel/IncommingCallAlert.js
+++ src/components/AlertModel/IncommingCallAlert.js
@@ -63,7 +63,7 @@ export default function IncomingCallAlert() {
 
       const returnUrl = `https://${shop}/apps/consultant-theme/consultant-dashboard`;
       const callUrl =
-        `${"https://test-consultation-app.zend-apps.comare.com"}/video/calling/page` +
+        `${"https://test-consultation-app.zend-apps.com"}/video/calling/page` +
         `?callerId=${callerId}` +
         `&receiverId=${userId}` +
         `&callType=${callType}` +
